3a1a4267123e024be1ceb3781c6d015cb1ebe56e
Stanisław Góra <stasgora@gmail.com>
2019-01-06 15:17:38 +0100

    migration to maven

---
+++ src/main/java/sgora/mesh/editor/AppController.java
@@ -0,0 +1,53 @@
+package sgora.mesh.editor;
+
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.scene.control.SplitPane;
+import javafx.scene.image.Image;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+import sgora.mesh.editor.canvas.ImgCanvas;
+
+import java.io.File;
+
+public class AppController {
+
+	private Stage stage;
+
+	public ImgCanvas canvas;
+	public AnchorPane canvasPane;
+	public SplitPane splitPane;
+
+	void init(Stage stage) {
+		this.stage = stage;
+
+		canvasPane.widthProperty().addListener((observable, oldVal, newVal) -> onCanvasAreaResized());
+		canvasPane.heightProperty().addListener((observable, oldVal, newVal) -> onCanvasAreaResized());
+		canvas.setOnScroll(event -> canvas.onScroll(event));
+		canvas.setOnMouseMoved(event -> canvas.onMouseMove(event));
+		canvas.setOnMouseDragged(event -> canvas.onMouseDrag(event));
+		canvas.setOnMousePressed(event -> canvas.onDragStarted(event));
+	}
+
+	private void onCanvasAreaResized() {
+		canvas.setWidth(canvasPane.getWidth());
+		canvas.setHeight(canvasPane.getHeight());
+	}
+
+	public void loadImage(ActionEvent event) {
+		FileChooser imageChooser = new FileChooser();
+		imageChooser.setTitle("Choose Image");
+		imageChooser.getExtensionFilters().addAll(new FileChooser.ExtensionFilter("Images", "*.jpg", "*.png", "*.bmp"));
+		File image = imageChooser.showOpenDialog(stage);
+
+		if(image == null)
+			return;
+		canvas.setBaseImage(new Image("file:" + image.getAbsolutePath()));
+	}
+
+	public void exitApp(ActionEvent event) {
+		Platform.exit();
+	}
+
+}
---
+++ src/main/java/sgora/mesh/editor/model/Point.java
@@ -0,0 +1,77 @@
+package sgora.mesh.editor.model;
+
+public class Point {
+
+	public double x, y;
+
+	public Point(double x, double y) {
+		this.x = x;
+		this.y = y;
+	}
+
+	public Point(Point point) {
+		this.x = point.x;
+		this.y = point.y;
+	}
+
+	public Point() {
+		this(0, 0);
+	}
+
+	public Point clamp(Point min, Point max) {
+		x = Math.max(min.x, Math.min(max.x, x));
+		y = Math.max(min.y, Math.min(max.y, y));
+		return this;
+	}
+
+	public Point clamp(Point max) {
+		return clamp(new Point(), max);
+	}
+
+	public Point min(Point other) {
+		x = Math.max(x, other.x);
+		y = Math.max(y, other.y);
+		return this;
+	}
+
+	@Override
+	public String toString() {
+		return "(" + x + ", " + y + ')';
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		return obj != null && obj.getClass().equals(this.getClass()) && x == ((Point) obj).x && y == ((Point) obj).y;
+	}
+
+	public Point multiplyByScalar(double amount) {
+		x *= amount;
+		y *= amount;
+		return this;
+	}
+
+	public Point multiply(Point point) {
+		x *= point.x;
+		y *= point.y;
+		return this;
+	}
+
+	public Point divide(Point point) {
+		x /= point.x;
+		y /= point.y;
+		return this;
+	}
+
+	public Point substract(Point point) {
+		x -= point.x;
+		y -= point.y;
+		return this;
+	}
+
+	public Point add(Point point) {
+		x += point.x;
+		y += point.y;
+		return this;
+	}
+
+}
